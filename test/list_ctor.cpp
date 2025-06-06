591d91365496da351ce132b7fc56bb891f93bd23
Hussein <husenalasady21@gmail.com>
2025-06-06 03:03:47 +0300

    Add list header

--- test/list_ctor.cpp
+++ test/list_ctor.cpp
@@ -1,17 +1,23 @@
 #include "list.h"
 #include "timer.h"
+#include <algorithm>
 #include <cstddef>
 #include <cstdio>
 
 int main()
 {
     aico::micro_timer time;
-
-    list<char> buffer(size_t(4096 * 1000)); 
-
-    buffer.mutate([](char& byte, [[maybe_unused]] size_t idx){byte = 0;});
     
-    auto interval = time.clock();
+    typedef list<char, 4, 1> str4;
+    str4 ABCD = {'A', 'B', 'C', 'D'};
+
+    list<str4> buffer(size_t(4096 * 1000));
+
+    std::fill(buffer.begin(), buffer.end(), ABCD);
+    
+    auto interval = time.time_since_start();
     printf("%li micro seconds.\n", interval.count());
+
+    buffer.print(std::cout);
     return 0;
 }
