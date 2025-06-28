0cf0f398b3311062cddedf896534ad3010967b3a
Hussein <husenalasady21@gmail.com>
2025-06-28 23:44:28 +0300

    Tests

---
+++ test/vec_indexing_test.cpp
@@ -0,0 +1,21 @@
+#include "vector.h"
+#include <iostream>
+
+int main()
+{
+    using namespace aico;
+    typedef vector<float, 2, true> vec2f;
+    
+    vec2f x;
+    x.at<0>() = 1.f;
+    x.at<1>() = 2.f;
+
+    for(auto f: x)
+        std::cout << f;
+
+    x[0]=3.f;
+    x[1]=4.f;
+    
+    for(auto f: x)
+        std::cout << f;
+}
