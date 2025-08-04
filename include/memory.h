d1877e87c0330cb7efefaaaa4feeb7cc07bd0d6d
Hussein <husenalasady21@gmail.com>
2025-08-04 23:48:55 +0300

    Add memory interface, untested

--- include/memory.h
+++ include/memory.h
@@ -1,41 +1,59 @@
 #pragma once
 
+#include "malc.h"
+
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
+#include <new>
 #include <type_traits>
+#include <utility>
 
 namespace aico::sys
 {
-    template<typename T=void>
-        requires(!std::is_void_v<T>)
-            [[nodiscard]] inline T* memalloc(
-        size_t bytes, 
-        size_t alignment=alignof(T))noexcept
-    {
-        return (T*)malloc(bytes);
-    }
-
-    [[nodiscard]] inline void* memalloc(size_t bytes,
-        size_t alignment=alignof(max_align_t))noexcept
-    {
-        return malloc(bytes);
-    }
-
-    template<typename T>
-        requires(!std::is_void_v<T>)
-    [[nodiscard]] inline T* cmemalloc(size_t count)noexcept
-    {
-        return memalloc<T>(sizeof(T)*count);
-    }
-
-    inline void memfree(void* addr)noexcept
-    {
-        free(addr);
-    }
-
     template<typename T, typename... Args>
-        requires(requires{T(std::declval<Args>()...);})
-    inline T* alloctr (size_t count=1, Args&&...args);
-    template<typename  T>
-    inline void dtrfree(T* addr);
+    requires(requires{T(std::declval<Args>()...);}&&!std::is_void_v<T>&&requires{~T();})
+    inline T* alloctr (size_t count=1, Args&&...args)
+    {
+        opres res;
+        auto addr=(T*)malc(sizeof(T)*count, 
+            alignof(T)>=alignof(void*)?alignof(T):alignof(void*), &res);
+        if(!(res==opres::SUCCESS&&addr)) throw std::bad_alloc();
+        size_t ctd=0;
+        for(T* it=addr; it<addr+count; ++it)
+            try{new (it) T(std::forward<Args>(args)...); ++ctd;}
+            catch(...)
+            {
+                for(T* end=addr+ctd; end!=addr;)
+                    (--end)->~T();
+                rel(addr);
+                throw;
+            }
+        return addr;
+    }
+    
+    //WARN: this function assumes memory is properly initialized
+    template<typename T>
+    requires(!std::is_void_v<T>&&requires{~T();})
+    inline void dtrel(T* addr) noexcept(std::is_nothrow_destructible_v<T>)
+    {
+        if constexpr(std::is_trivially_destructible_v<T>)
+        {
+            rel(addr);
+            return;
+        }
+        auto hdr=gethdr(addr);
+        if(!hdr) return; //the fuck?
+        //XXX this is fragile, relies on hdr_t too much, consider storing obj count
+        //in metadata directly
+        size_t nrTs=(hdr->bytes-((uintptr_t)addr-(uintptr_t)hdr->base))/sizeof(T);
+        for(T* end=addr+nrTs; end!=addr;)
+            try{(--end)->~T();}
+            catch(...)
+            {
+                rel(addr); //dumb user, eat memory leaks
+                throw;
+            }
+        rel(addr);
+    }
 }
